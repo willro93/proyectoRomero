176f7e9ea565d4164d9fd9f088ad854aeddac07e
Will <110594293+willro93@users.noreply.github.com>
2023-07-04 20:32:50 -0600

    primera_entrega_Romero

---
+++ proyectoRomero.js
@@ -0,0 +1,68 @@
+class ProductManager {
+  constructor() {
+    this.products = [];
+  }
+
+  idGenerator = () => {
+    const count = this.products.length;
+    if (count === 0) {
+      return 1;
+    } else {
+      return this.products[count - 1].id + 1;
+    }
+  };
+
+  addProduct = (title, description, price, thumbnail, code, stock) => {
+    const id = this.idGenerator();
+
+    this.products.push({
+      id,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    });
+  };
+
+  getProductById = () => {
+    if (this.products.find((element) => element.id === this.idGenerator())) {
+      console.log("El producto ha sido encontrado");
+    } else {
+      console.log("Not found");
+    }
+  };
+
+  getProduct = () => {
+    return this.products;
+  };
+}
+
+const productManager = new ProductManager();
+productManager.addProduct(
+  "shampoo sólido",
+  "El mejor shampoo",
+  "$150.00",
+  "img1",
+  "shamp1",
+  "500 unidades"
+);
+productManager.addProduct(
+  "Acondicionador sólido",
+  "Para cabello seco",
+  "$140.00",
+  "img2",
+  "aco1",
+  "700 unidades"
+);
+productManager.addProduct(
+  "shampoo sólido 2",
+  "El mejor shampoo 2",
+  "$150.00",
+  "img1",
+  "shamp1",
+  "500 unidades"
+);
+console.log(productManager.getProduct());
+console.log(productManager.getProductById());
